ba9638648f83df1eec610230741d9be7cf162c2f
Lesly Sanchez <leslyf.sanchezm@gmail.com>
2020-05-08 22:43:06 -0500

    adding Firebase to the script

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Initialize Firebase 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBUC6fC1dnj9fCIAqowYuwUlzsvknbyUnE",
+    authDomain: "the-right-swipe-409f9.firebaseapp.com",
+    databaseURL: "https://the-right-swipe-409f9.firebaseio.com",
+    projectId: "the-right-swipe-409f9",
+    storageBucket: "the-right-swipe-409f9.appspot.com",
+    messagingSenderId: "1048291636935",
+    appId: "1:1048291636935:web:32b88ef6288ab9608130e2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
